5aa21aead6637f78cda4a18ec537decaec252a50
Pregrado <Pregrado>
2016-03-31 12:01:51 -0500

    ojala suba

---
+++ readme.md
@@ -0,0 +1,6 @@
+ejemplo de uso del microframework web para la implementacion de microservicios 
+(microservices)
+
+python=2.7
+flask=0.10.1
+
---
+++ usuarios.py
@@ -0,0 +1,17 @@
+
+import json
+import commands
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+
+	ret = commands.getoutput("ls /home | sort")
+	ret = ret.split("\n")
+	ret = "{users:"+json.dumps(ret)+"}"
+	return ret
+
+if __name__ == "__main__":
+    app.run('0.0.0.0',debug=True)
+
